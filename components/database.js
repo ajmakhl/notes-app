6ce513d9a2c91a8e9e8f0c93fea1ce42c47d18d2
AJ Makhl <ajmakhl@MacBook-Pro.local>
2020-04-16 02:22:38 -0400

    attempt 4 - Unexpected end of JSON input

--- components/database.js
+++ components/database.js
@@ -1,12 +1,12 @@
-import _ from "lodash";
+// import _ from "lodash";
 const key = "notes-app-q1!w2@";
 export const post = (note) => {
-  const db = JSON.parse(localStorage.getItem(key) || []);
+  const db = JSON.parse(localStorage.getItem(key)) || [];
   localStorage.setItem(key, JSON.stringify([...db, note]));
 };
 export const get = () => {
-  const db = JSON.parse(localStorage.getItem(key) || []);
-  const sorted = _.sortBy(db, "createdAt");
+  const db = JSON.parse(localStorage.getItem(key)) || [];
+  // const sorted = _.sortBy(db, "createdAt");
   return sorted.reverse();
 };
 export const remove = (note) => {
